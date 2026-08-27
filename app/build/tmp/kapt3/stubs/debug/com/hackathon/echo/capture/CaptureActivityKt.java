4b9d334a26fa600a6562fe4b04f77b481935ec5b
Saurav-Biswal <biswalsameer077@gmail.com>
2026-08-27 12:23:39 +0530

    Revert "Add manual text and link input"

--- app/build/tmp/kapt3/stubs/debug/com/hackathon/echo/capture/CaptureActivityKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/hackathon/echo/capture/CaptureActivityKt.java
@@ -1,11 +1,10 @@
 package com.hackathon.echo.capture;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u001e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a;\u0010\u0000\u001a\u00020\u00012\u001e\b\u0002\u0010\u0002\u001a\u0018\b\u0001\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00010\u0004\u0012\u0006\u0012\u0004\u0018\u00010\u00050\u00032\f\u0010\u0006\u001a\b\u0012\u0004\u0012\u00020\u00010\u0007H\u0007\u00a2\u0006\u0002\u0010\b\u00a8\u0006\t"}, d2 = {"EchoCaptureOverlay", "", "awaitProcessing", "Lkotlin/Function1;", "Lkotlin/coroutines/Continuation;", "", "onComplete", "Lkotlin/Function0;", "(Lkotlin/jvm/functions/Function1;Lkotlin/jvm/functions/Function0;)V", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u000e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u0016\u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00010\u0003H\u0007\u00a8\u0006\u0004"}, d2 = {"EchoCaptureOverlay", "", "onComplete", "Lkotlin/Function0;", "app_debug"})
 public final class CaptureActivityKt {
     
     @androidx.compose.runtime.Composable()
     public static final void EchoCaptureOverlay(@org.jetbrains.annotations.NotNull()
-    kotlin.jvm.functions.Function1<? super kotlin.coroutines.Continuation<? super kotlin.Unit>, ? extends java.lang.Object> awaitProcessing, @org.jetbrains.annotations.NotNull()
     kotlin.jvm.functions.Function0<kotlin.Unit> onComplete) {
     }
 }
